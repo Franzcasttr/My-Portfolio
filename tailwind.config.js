80da5a54fc82b7f25655da8ee65cc5719887d4a7
Franz <franiscatro.216.fc@gmail.com>
2023-01-09 15:43:13 +0800

    fixed tailwind v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './assets/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -11,10 +12,6 @@ module.exports = {
         '4xl': [
           '5px 5px 5px rgba(0, 0, 0, 0.25)',
           '-5px -5px 5px rgba(255, 255, 255, 0.15)',
-        ],
-        '5xl': [
-          'inset -3px -2px 5px rgba(255, 255, 255, 0.25)',
-          'inset 1px 1px 10px rgba(0, 0, 0, 0.25)',
         ],
       },
     },
